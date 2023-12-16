799bb16a2f6c9fdde8c03c5c7938e6dc06032f55
gerel <gerel@bk.ru>
2023-12-16 18:22:40 +0500

    Add Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет
+// местами первую и последнюю строку массива.
