bbf19cf2271b1ea8b74b817f9e13f2e0c99ceb77
gerel <gerel@bk.ru>
2023-12-16 18:24:17 +0500

    Add standart methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет
 // местами первую и последнюю строку массива.
+int [,] FillArray(int columns, int rows, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = rnd.Next(minValue, maxValue+1);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int [,] arr = FillArray(5, 5, 0, 10);
+PrintArray(arr);
